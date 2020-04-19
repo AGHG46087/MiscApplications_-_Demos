49402180d4a4d45a252cfc59fa49a2c36a99f022
Hans-Jurgen Greiner <grein46087@icloud.com>
2020-04-19 09:03:15 -0700

    Added in a PHP to query a result

---
+++ zzz_MySite_Archive/queryresults.php
@@ -0,0 +1,11 @@
+<?php
+
+$q=$_GET["q"];
+
+echo '<strong>Han</strong>s-Jurgen Greiner,';
+echo  'My<strong>Han</strong>over,';
+echo  'chris <strong>Han</strong>son,';
+echo '<strong>Han</strong>s Chritian Anderson,';
+echo 'Corporation <strong>Han</strong>son,';
+
+?>
